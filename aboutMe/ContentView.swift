55b30af277b5889b8a2490ce5150689829e1140d
Nikita <nikicollegeapp@gmail.com>
2023-07-14 19:46:37 +0530

    Initial Commit

---
+++ aboutMe/ContentView.swift
@@ -0,0 +1,69 @@
+//
+//  ContentView.swift
+//  aboutMe
+//
+//  Created by Nikita Soundar Rajan on 14/07/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    @State private var myFact = ""
+    var body: some View {
+        ZStack{
+            Color(.systemGray)
+                .ignoresSafeArea()
+        
+            VStack {
+                Spacer()
+                Text("🍃🕊☘☁️🍈💚")
+                Text("Meet, Nikita!")
+                    .font(.title)
+                    .fontWeight(.bold)
+                    .foregroundColor(Color.white)
+                Spacer()
+                // make the meet nikita diff color
+                
+                Image("me")
+                    .resizable(resizingMode: .stretch)
+                    .aspectRatio(contentMode: .fit)
+                    .frame(height: 450.0)
+                    .cornerRadius(20)
+                Spacer()
+                
+                Button("Fun fact!")
+                {myFact = "I have published my first poetry book and have written and sung a song!"
+                }
+                .font(.title2)
+                .buttonStyle(.borderedProminent)
+                .tint(.green)
+                .bold()
+                .fontWeight(/*@START_MENU_TOKEN@*/.bold/*@END_MENU_TOKEN@*/)
+                
+                Text(myFact)
+                    .font(.body)
+                
+                    .fontWeight(.heavy)
+                    .foregroundColor(Color.white)
+                    .multilineTextAlignment(.center)
+                    .bold()
+                    .padding(25.0)
+                
+                Spacer()
+                Spacer()
+                
+                
+                
+                
+            }
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+
+
---
+++ aboutMe/aboutMeApp.swift
@@ -0,0 +1,17 @@
+//
+//  aboutMeApp.swift
+//  aboutMe
+//
+//  Created by Nikita Soundar Rajan on 14/07/23.
+//
+
+import SwiftUI
+
+@main
+struct aboutMeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
